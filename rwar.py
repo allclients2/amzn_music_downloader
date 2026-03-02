ff3c3f34961c1bd73cd95c527ad7dc703186dfaa
brend <brendan.viravongnew@gmail.com>
2026-03-02 00:29:10 -0500

    clean workspace

--- rwar.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-# Generate a random UUID (version 4)
-my_uuid = uuid.uuid4()
-print(f"UUID object: {my_uuid}")
-
-# Convert the UUID object to a string
-my_uuid_string = str(uuid.uuid4())
-print(f"UUID string: {my_uuid_string}")
